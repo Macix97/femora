b793b6fa06cafef47cdfc9d203dfaa84f2dcb474
Macix97 <macix97@gmail.com>
2020-10-15 20:13:36 +0200

    Upgrading LWRP to SRP (graphics and light improvements)

--- Scripts/PeriodManager.cs
+++ Scripts/PeriodManager.cs
@@ -3,44 +3,35 @@ using System.Collections.Generic;
 
 public class PeriodManager : MonoBehaviour
 {
-    // Sun
-    public static readonly string Sun = "Sun";
-    // Day skybox
-    public static readonly string Day = "Day";
-    // Night skybox
-    public static readonly string Night = "Night";
-    // Period modifier
-    private float _periodMod = 360f;
-    // No light
-    private float _noLight = 0f;
-    // Poor light
-    private float _poorLight = 0.2f;
-    // Weak light
-    private float _weakLight = 0.4f;
-    // Average light
-    private float _averageLight = 0.6f;
-    // Strong light
-    private float _strongLight = 0.8f;
-    // Mighty light
-    private float _mightyLight = 1f;
+    // Day color
+    public Color DayColor;
+    // Night color
+    public Color NightColor;
+    // Time factor (the bigger the longer the day)
+    [Range(10, 720)]
+    public int TimeFac;
     // Sun light
     private Light _sun;
-    // Lightmap data
-    private LightmapData[] _lightMaps;
-    // Check if is day
-    private bool _isDay;
-    // Day skybox
-    private Material _daySkybox;
-    // Night skybox
-    private Material _nightSkybox;
+    // Skybox
+    private Material _skybox;
     // Torches
     private ParticleSystem[] _torches;
     // Campfires
     private ParticleSystem[] _campfires;
     // Infernal lanterns
     private ParticleSystem[] _infernalLanterns;
+    // Check if is day
+    private bool _isDay;
     // Check if fire is active
     private bool _isFire;
+    // Skybox exposure
+    private float _exposure;
+    // Light color differences
+    private float _rDiff;
+    private float _gDiff;
+    private float _bDiff;
+    // Hero lamp
+    private Light _lamp;
 
     // Awake is called when the script instance is being loaded
     private void Awake()
@@ -57,13 +48,15 @@ public class PeriodManager : MonoBehaviour
     // Set basic parameters
     private void Init()
     {
-        _sun = GameObject.Find(Sun).GetComponent<Light>();
-        _lightMaps = new LightmapData[1];
-        _lightMaps[0] = new LightmapData();
-        AdaptLightmaps(PeriodDatabase.Noon);
-        _sun.intensity = 1f;
-        _daySkybox = Resources.Load<Material>(ItemDatabase.Materials + Day);
-        _nightSkybox = Resources.Load<Material>(ItemDatabase.Materials + Night);
+        _sun = GameObject.Find("Sun").GetComponent<Light>();
+        _sun.intensity = _exposure = 1f;
+        _lamp = GameObject.FindGameObjectWithTag(HeroClass.HeroTag).GetComponentInChildren<Light>();
+        _lamp.intensity = 0f;
+        _skybox = Resources.Load<Material>("Skybox/Skybox");
+        _skybox.SetFloat("_Exposure", _exposure);
+        _rDiff = Mathf.Abs(DayColor.r - NightColor.r);
+        _gDiff = Mathf.Abs(DayColor.g - NightColor.g);
+        _bDiff = Mathf.Abs(DayColor.b - NightColor.b);
         List<ParticleSystem> torches = new List<ParticleSystem>();
         List<ParticleSystem> campfires = new List<ParticleSystem>();
         List<ParticleSystem> infernalLanterns = new List<ParticleSystem>();
@@ -92,75 +85,92 @@ public class PeriodManager : MonoBehaviour
         _torches = torches.ToArray();
         _campfires = campfires.ToArray();
         _infernalLanterns = infernalLanterns.ToArray();
-        RenderSettings.skybox = _daySkybox;
-        _isDay = true;
         _isFire = false;
+        _isDay = true;
     }
 
     // Adapt period to time
     private void AdaptPeriod()
     {
+        // Get current sky color
+        Color currentColor = RenderSettings.ambientSkyColor;
         // Is day
         if (_isDay)
         {
-            // Is day
-            if (_sun.intensity > 0)
-                // Decrease sun intensity
-                _sun.intensity -= Time.deltaTime / _periodMod;
-            // Is night
-            else
+            // Check light color
+            if (currentColor.r < NightColor.r || currentColor.g < NightColor.g || currentColor.b < NightColor.b)
             {
-                // disable light
+                // Set night color
+                currentColor.r = NightColor.r;
+                currentColor.g = NightColor.g;
+                currentColor.b = NightColor.b;
+            }
+            // Check light intensity
+            if (_sun.intensity < 0f || _exposure < 0.1f)
+            {
+                // Switch time of day
                 _sun.intensity = 0;
-                // Set that is night
+                _exposure = 0.1f;
                 _isDay = false;
             }
+            // Check light intensity
+            if (_sun.intensity < 0.5f)
+            {
+                // Check lamp intensity
+                if (_lamp.intensity > 1f)
+                    _lamp.intensity = 1f;
+                // Increase lamp intensity
+                _lamp.intensity += Time.deltaTime / (TimeFac / 2f);
+            }
+            // Decrease light intensity 
+            _sun.intensity -= Time.deltaTime / TimeFac;
+            // Decrease exposure
+            _exposure -= Time.deltaTime / (TimeFac + (TimeFac * 0.1f));
+            _skybox.SetFloat("_Exposure", _exposure);
+            // Change sky color
+            currentColor.r -= Time.deltaTime / (TimeFac / _rDiff);
+            currentColor.g -= Time.deltaTime / (TimeFac / _gDiff);
+            currentColor.b -= Time.deltaTime / (TimeFac / _bDiff);
         }
         // Is night
         else
         {
-            // Is night
-            if (_sun.intensity < 1)
-                // Increase sun intensity
-                _sun.intensity += Time.deltaTime / _periodMod;
-            // Is night
-            else
+            // Check light color
+            if (currentColor.r > DayColor.r || currentColor.g > DayColor.g || currentColor.b > DayColor.b)
             {
-                // enable light
+                // Set day color
+                currentColor.r = DayColor.r;
+                currentColor.g = DayColor.g;
+                currentColor.b = DayColor.b;
+            }
+            // Check light intensity
+            if (_sun.intensity > 1f || _exposure > 1f)
+            {
+                // Switch time of day
                 _sun.intensity = 1;
-                // Set that is day
                 _isDay = true;
             }
+            // Check light intensity
+            if (_sun.intensity < 0.5f)
+            {
+                // Check lamp intensity
+                if (_lamp.intensity < 0f)
+                    _lamp.intensity = 0f;
+                // Decrease lamp intensity
+                _lamp.intensity -= Time.deltaTime / (TimeFac / 2f);
+            }
+            // Increase light intensity
+            _sun.intensity += Time.deltaTime / TimeFac;
+            // Increase exposure
+            _exposure += Time.deltaTime / (TimeFac + (TimeFac * 0.1f));
+            _skybox.SetFloat("_Exposure", _exposure);
+            // Change sky color
+            currentColor.r += Time.deltaTime / (TimeFac / _rDiff);
+            currentColor.g += Time.deltaTime / (TimeFac / _gDiff);
+            currentColor.b += Time.deltaTime / (TimeFac / _bDiff);
         }
-        // Set proper skybox
-        if (_sun.intensity < _weakLight)
-            // Set night skybox
-            RenderSettings.skybox = _nightSkybox;
-        // Set day skybox
-        else
-            RenderSettings.skybox = _daySkybox;
-        // Midnight
-        if (_sun.intensity >= _noLight && _sun.intensity <= _poorLight)
-            // Set proper lightmaps
-            AdaptLightmaps(PeriodDatabase.Midnight);
-        // Evening
-        else if (_sun.intensity > _poorLight && _sun.intensity <= _weakLight)
-            // Set proper lightmaps
-            AdaptLightmaps(PeriodDatabase.Evening);
-        // Afternoon
-        else if (_sun.intensity > _weakLight && _sun.intensity <= _averageLight)
-            // Set proper lightmaps
-            AdaptLightmaps(PeriodDatabase.Afternoon);
-        // Morning
-        else if (_sun.intensity > _averageLight && _sun.intensity <= _strongLight)
-            // Set proper lightmaps
-            AdaptLightmaps(PeriodDatabase.Morning);
-        // Noon
-        else if (_sun.intensity > _strongLight && _sun.intensity <= _mightyLight)
-            // Set proper lightmaps
-            AdaptLightmaps(PeriodDatabase.Noon);
         // Enable fire
-        if (_sun.intensity >= _noLight && _sun.intensity <= _poorLight)
+        if (_sun.intensity < 0.25f)
         {
             // Check if fire is active
             if (!_isFire)
@@ -239,18 +249,7 @@ public class PeriodManager : MonoBehaviour
                 _isFire = false;
             }
         }
-    }
-
-    // Adapt lightmaps
-    private void AdaptLightmaps(string period)
-    {
-        // Get proper lightmaps color
-        _lightMaps[0].lightmapColor = PeriodDatabase.GetProperPeriod(period).Color;
-        // Get proper lightmaps direction
-        _lightMaps[0].lightmapDir = PeriodDatabase.GetProperPeriod(period).Direction;
-        // Set new lightmaps
-        LightmapSettings.lightmaps = _lightMaps;
-        // Set new reflection
-        RenderSettings.customReflection = PeriodDatabase.GetProperPeriod(period).Reflection;
+        // Set sky color
+        RenderSettings.ambientSkyColor = new Color(currentColor.r, currentColor.g, currentColor.b);
     }
 }
